059ac9d6cd48786f4aa7f5d3eee9e40dffb2953b
blacksmok101-cmd <blacksmok101@gmail.com>
2026-01-26 19:08:30 +0400

    .

--- src/main.jsx
+++ src/main.jsx
@@ -8,8 +8,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <App />
   </React.StrictMode>
 );
-
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@500;600;700&family=Inter:wght@400;500;600&display=swap" rel="stylesheet">
-
