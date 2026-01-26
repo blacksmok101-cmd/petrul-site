67e6222881bc1e22281b5f5ef66fef6612242166
blacksmok101-cmd <blacksmok101@gmail.com>
2026-01-27 01:32:29 +0400

    .

--- src/components/SocialBar.jsx
+++ src/components/SocialBar.jsx
@@ -4,7 +4,7 @@ export default function SocialBar() {
       <div className="socialInner">
         <button className="btn disabled" disabled title="Closed for now">Telegram (Closed)</button>
         <button className="btn disabled" disabled title="Closed for now">Discord (Closed)</button>
-        <a className="btn primary" href="https://x.com/Petrulom" target="_blank" rel="noreferrer">Twitter / X</a>
+        <a className="btn primary" href="https://x.com/Petrulom" target="_blank" rel="noreferrer">X</a>
       </div>
     </div>
   );
